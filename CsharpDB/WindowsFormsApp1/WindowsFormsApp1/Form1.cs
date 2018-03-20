62c48088bc5e20cad89eadb3d32fa3cc9630cfe3
David <8584751@qq.com>
2018-03-20 16:11:13 +0800

    周二下午班级

--- CsharpDB/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ CsharpDB/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -28,11 +29,21 @@ namespace WindowsFormsApp1
             username = textBox1.Text;
             password = textBox2.Text;
             //textBox2.Text = textBox1.Text;
-            if (username == "abc" && password == "123")
+            string myConn = "Data Source=.;Initial Catalog=Test;User ID=sa;Password=sql";
+            SqlConnection sqlConnection = new SqlConnection(myConn);  //实例化连接对象
+            sqlConnection.Open();
+
+            string sql = "select userid,password from usertable where userid = '" + username + "' and password = '" + password + "'";                                            //编写SQL命令
+            SqlCommand sqlCommand = new SqlCommand(sql, sqlConnection);
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            if (sqlDataReader.HasRows)
             {
                 MessageBox.Show("登陆成功！");
                 Form2 form2 = new Form2();
                 form2.Show();
+                this.Hide();
             }
             else
                 MessageBox.Show("失败！");
