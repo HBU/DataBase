1e3f559e8f63a4db991c2b6c0c2f1e9a3a61ee2b
David <8584751@qq.com>
2018-03-24 21:46:13 +0800

    Update CREATE-TABLE-Course.sql

--- SQL/CREATE-TABLE-Course.sql
+++ SQL/CREATE-TABLE-Course.sql
@@ -1,1 +1,7 @@
-	CREATE TABLE  Course          (Cno       CHAR(4) PRIMARY KEY,        	   Cname  CHAR(40),                     	  Cpno     CHAR(4),               	                                  Ccredit  SMALLINT,            FOREIGN KEY (Cpno) REFERENCES  Course(Cno)           ); 
+CREATE TABLE  Course
+(Cno       CHAR(4) PRIMARY KEY,
+Cname  CHAR(40),            
+Cpno     CHAR(4),               	                      
+Ccredit  SMALLINT,
+FOREIGN KEY (Cpno) REFERENCES  Course(Cno) 
+); 
