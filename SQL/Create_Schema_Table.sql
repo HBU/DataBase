f848eddb39dbf880bab2febf30b6d3c62c8420b5
David <8584751@qq.com>
2018-03-24 21:42:12 +0800

    Update Create_Schema_Table.sql

--- SQL/Create_Schema_Table.sql
+++ SQL/Create_Schema_Table.sql
@@ -1,1 +1,9 @@
-CREATE SCHEMA TEST AUTHORIZATION WANGCREATE TABLE TAB1   ( COL1 SMALLINT,                                             COL2 INT,                                            COL3 CHAR(20),                                            COL4 NUMERIC(10,3),                                            COL5 DECIMAL(5,2)                                          );
+CREATE SCHEMA TEST AUTHORIZATION WANG
+CREATE TABLE TAB1   
+( 
+COL1 SMALLINT, 
+COL2 INT,
+COL3 CHAR(20),
+COL4 NUMERIC(10,3),
+COL5 DECIMAL(5,2)
+);
