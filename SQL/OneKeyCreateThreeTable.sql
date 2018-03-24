765fdada85e1f5937a9b7c6efa8c3d807a8bbe7c
David <8584751@qq.com>
2018-03-24 17:25:35 +0800

    SQL

---
+++ SQL/OneKeyCreateThreeTable.sql
@@ -0,0 +1,44 @@
+--Edit by David @ HeBei University 2018
+
+USE MrDavid  
+GO 
+
+DROP TABLE IF EXISTS SC
+DROP TABLE IF EXISTS Student
+DROP TABLE IF EXISTS Course
+
+CREATE TABLE Student          
+      (	Sno   CHAR(9) PRIMARY KEY,         /* 列级完整性约束条件,Sno是主码*/                  
+        Sname CHAR(20) UNIQUE,             /* Sname取唯一值*/
+        Ssex  CHAR(2),
+        Sage  SMALLINT,
+        Sdept CHAR(20)
+      ); 
+
+CREATE TABLE  Course
+      (	Cno		CHAR(4) PRIMARY KEY,
+		Cname	CHAR(40),            
+        Cpno	CHAR(4),               	                      
+        Ccredit	SMALLINT,
+        FOREIGN KEY (Cpno) REFERENCES  Course(Cno) 
+       ); 
+
+CREATE TABLE  SC
+       (Sno		CHAR(9), 
+        Cno		CHAR(4),  
+        Grade	SMALLINT,
+        PRIMARY KEY (Sno,Cno),                     /* 主码由两个属性构成，必须作为表级完整性进行定义*/
+        FOREIGN KEY (Sno) REFERENCES Student(Sno),  /* 表级完整性约束条件，Sno是外码，被参照表是Student */
+        FOREIGN KEY (Cno)REFERENCES Course(Cno)     /* 表级完整性约束条件， Cno是外码，被参照表是Course*/
+        ); 
+
+
+INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage)    VALUES ('201215121','李勇','男','CS',20);
+INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage)    VALUES ('201215122','刘晨','女','CS',19);
+INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage)    VALUES ('201215123','王敏','女','MA',18);
+INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage)    VALUES ('201215125','张立','男','IS',19);
+INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage)    VALUES ('201215128','陈冬','男','IS',20);
+
+SELECT * FROM Student
+
+INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('1','数据库',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('2','数学',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('3','信息系统',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('4','操作系统',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('5','数据结构',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('6','数据处理',NULL,4);INSERT  INTO Course(Cno,Cname,Cpno,Ccredit)	VALUES ('7','Pascal语言',NULL,4);UPDATE Course SET Cpno = '5' WHERE Cno = '1' UPDATE Course SET Cpno = '1' WHERE Cno = '3' UPDATE Course SET Cpno = '6' WHERE Cno = '4' UPDATE Course SET Cpno = '7' WHERE Cno = '5' UPDATE Course SET Cpno = '6' WHERE Cno = '7' SELECT * FROM Course
