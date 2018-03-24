72a399f608b3df5c6112bf67d0ed2af1f4e02e3b
David <8584751@qq.com>
2018-03-24 22:38:41 +0800

    utf8

--- SQL/OneKeyCreateThreeTable.sql
+++ SQL/OneKeyCreateThreeTable.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS Course
 
 CREATE TABLE Student          
  (	
- Sno CHAR(9) PRIMARY KEY,         /* 列级完整性约束条件,Sno是主码*/                  
+ Sno CHAR(9) PRIMARY KEY,        /* 列级完整性约束条件,Sno是主码*/                  
  Sname CHAR(20) UNIQUE,             /* Sname取唯一值*/
  Ssex CHAR(2),
  Sage SMALLINT,
