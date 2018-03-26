da3db63924f2024c3a6e367e04e5bfdd5143cbd4
David <8584751@qq.com>
2018-03-26 15:44:28 +0800

    Update OneKeyCreateThreeTable.sql

--- SQL/OneKeyCreateThreeTable.sql
+++ SQL/OneKeyCreateThreeTable.sql
@@ -30,7 +30,7 @@ CREATE TABLE  SC
  PRIMARY KEY (Sno,Cno),                     /* 主码由两个属性构成，必须作为表级完整性进行定义*/
  FOREIGN KEY (Sno) REFERENCES Student(Sno),  /* 表级完整性约束条件，Sno是外码，被参照表是Student */
  FOREIGN KEY (Cno)REFERENCES Course(Cno)     /* 表级完整性约束条件， Cno是外码，被参照表是Course*/
-        ); 
+ ); 
 
 
 INSERT  INTO  Student (Sno,Sname,Ssex,Sdept,Sage) VALUES ('201215121','李勇','男','CS',20);
