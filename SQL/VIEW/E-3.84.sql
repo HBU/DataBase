5f7799f88534ac1db903d3e217419d778e7fa9ca
David <8584751@qq.com>
2018-03-28 12:29:55 +0800

    view

--- SQL/VIEW/E-3.84.sql
+++ SQL/VIEW/E-3.84.sql
@@ -1,1 +1,1 @@
-CREATE VIEW IS_StudentAS 	SELECT Sno,Sname,Sage	FROM     Student	WHERE  Sdept= 'IS';SELECT * FROM IS_Student
+﻿GOCREATE VIEW IS_StudentAS 	SELECT Sno,Sname,Sage	FROM     Student	WHERE  Sdept= 'IS';GOSELECT * FROM IS_Student
---
+++ SQL/VIEW/E-3.94.sql
@@ -0,0 +1,1 @@
+SELECT *FROM  (SELECT Sno,AVG(Grade)		       FROM  SC  		       GROUP BY Sno) AS S_G(Sno,Gavg)WHERE Gavg>=86;
