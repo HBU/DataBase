899439b400aa4f3bc9843cae9e39dae0c967515a
David <8584751@qq.com>
2018-03-28 12:38:09 +0800

    Update E-3.49.sql

--- SQL/SELECT/E-3.49.sql
+++ SQL/SELECT/E-3.49.sql
@@ -1,17 +1,17 @@
 
---µÈÖµÁ¬½Ó
+--等值连接
 SELECT  Student.*, SC.*
 FROM     Student, SC
 WHERE  Student.Sno = SC.Sno;
 
 
---×ÔÈ»Á¬½Ó
+--自然连接
 SELECT  Student.Sno,Sname,Ssex,Sage,Sdept,Cno,Grade
 FROM     Student,SC
 WHERE  Student.Sno = SC.Sno;
 
 
---µÈÖµÁ¬½Ó
+--等值连接
 SELECT  *
 FROM     Student, SC
 WHERE  Student.Sno = SC.Sno;
