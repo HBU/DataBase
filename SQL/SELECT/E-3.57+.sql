d233568427f386ea3691e8a92d84ee8d83d1473d
David <8584751@qq.com>
2018-03-28 12:41:53 +0800

    Update E-3.57+.sql

--- SQL/SELECT/E-3.57+.sql
+++ SQL/SELECT/E-3.57+.sql
@@ -1,1 +1,12 @@
-SELECT Sno, CnoFROM SC, (SELECT  Sno, Avg(Grade) 		FROM   SC 		GROUP BY   Sno)		AS   Avg_sc(avg_sno,avg_grade)WHERE SC.Sno = Avg_sc.avg_sno and SC.Grade >=Avg_sc.avg_gradeSELECT SnameFROM Student,      (SELECT Sno FROM SC WHERE Cno='1') AS SC1WHERE  Student.Sno=SC1.Sno;
+SELECT Sno, Cno
+FROM SC, (SELECT  Sno, Avg(Grade) 
+		FROM   SC 
+		GROUP BY   Sno)
+		AS   Avg_sc(avg_sno,avg_grade)
+WHERE SC.Sno = Avg_sc.avg_sno and SC.Grade >=Avg_sc.avg_grade
+
+
+SELECT Sname
+FROM Student,  
+    (SELECT Sno FROM SC WHERE Cno='1') AS SC1
+WHERE  Student.Sno=SC1.Sno;
