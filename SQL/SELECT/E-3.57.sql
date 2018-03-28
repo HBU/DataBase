8a90f352f9fe284a09a9121dc6613cbe0ee4f706
David <8584751@qq.com>
2018-03-28 12:41:58 +0800

    Update E-3.57.sql

--- SQL/SELECT/E-3.57.sql
+++ SQL/SELECT/E-3.57.sql
@@ -1,1 +1,6 @@
-SELECT Sno, CnoFROM    SC  xWHERE Grade >=(	SELECT AVG(Grade) 				FROM  SC y				WHERE y.Sno=x.Sno);
+
+SELECT Sno, Cno
+FROM    SC  x
+WHERE Grade >=(	SELECT AVG(Grade) 
+				FROM  SC y
+				WHERE y.Sno=x.Sno);
