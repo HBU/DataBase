c15bb8ca72f1feab72f0f0cab4f8f75bee36b27d
David <8584751@qq.com>
2018-03-28 12:31:16 +0800

    Update E-3.94.sql

--- SQL/VIEW/E-3.94.sql
+++ SQL/VIEW/E-3.94.sql
@@ -1,1 +1,5 @@
-SELECT *FROM  (SELECT Sno,AVG(Grade)		       FROM  SC  		       GROUP BY Sno) AS S_G(Sno,Gavg)WHERE Gavg>=86;
+SELECT *
+FROM  (SELECT Sno,AVG(Grade)
+       FROM  SC
+       GROUP BY Sno) AS S_G(Sno,Gavg)
+WHERE Gavg>=86;
