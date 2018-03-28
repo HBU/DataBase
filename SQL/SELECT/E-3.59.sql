a1b9e0136c0969ec2e34c9b35b22f484c4f0ea20
David <8584751@qq.com>
2018-03-28 12:42:56 +0800

    Update E-3.59.sql

--- SQL/SELECT/E-3.59.sql
+++ SQL/SELECT/E-3.59.sql
@@ -1,1 +1,17 @@
-SELECT Sname,SageFROM    StudentWHERE Sage < ALL (	SELECT  Sage					FROM    Student					WHERE Sdept= 'CS')     AND Sdept <> 'CS';           /*父查询块中的条件 */SELECT Sname,SageFROM   StudentWHERE Sage < 			(SELECT MIN(Sage)			FROM Student			WHERE Sdept= 'CS ')       AND Sdept <> 'CS';SELECT * FROM Student
+SELECT Sname,Sage
+FROM    Student
+WHERE Sage < ALL (	SELECT  Sage
+					FROM    Student
+					WHERE Sdept= 'CS')
+     AND Sdept <> 'CS';           /*鐖舵煡璇㈠潡涓殑鏉′欢*/
+
+
+SELECT Sname,Sage
+FROM   Student
+WHERE Sage < 
+			(SELECT MIN(Sage)
+			FROM Student
+			WHERE Sdept= 'CS ')
+       AND Sdept <> 'CS';
+
+SELECT * FROM Student
