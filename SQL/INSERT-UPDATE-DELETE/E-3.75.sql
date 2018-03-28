d91d1a058f04eddefa64ee2c3f44af840cfe40a3
David <8584751@qq.com>
2018-03-28 12:35:22 +0800

    Update E-3.75.sql

--- SQL/INSERT-UPDATE-DELETE/E-3.75.sql
+++ SQL/INSERT-UPDATE-DELETE/E-3.75.sql
@@ -1,1 +1,8 @@
-SELECT * FROM SCUPDATE SCSET     Grade=1WHERE Sno  IN	(SELECT Sno	FROM     Student	WHERE  Sdept= 'CS' );SELECT * FROM SC
+SELECT * FROM SC
+UPDATE SC
+SET     Grade=1
+WHERE Sno  IN
+	(SELECT Sno
+	FROM     Student
+	WHERE  Sdept= 'CS' );
+SELECT * FROM SC
