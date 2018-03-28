cd25286e8ec556175d382080aa239711ba0798b7
David <8584751@qq.com>
2018-03-28 12:40:59 +0800

    Update E-3.54.sql

--- SQL/SELECT/E-3.54.sql
+++ SQL/SELECT/E-3.54.sql
@@ -1,8 +1,8 @@
 SELECT Student.Sno, Sname, Cname, Grade
-FROM Student, SC, Course    /*¶à±íÁ¬½Ó*/
+FROM Student, SC, Course    /*/*多表连接*/*/
 WHERE Student.Sno = SC.Sno AND SC.Cno = Course.Cno;
 
 
 SELECT *
-FROM Student, SC, Course    /*¶à±íÁ¬½Ó*/
+FROM Student, SC, Course    /*/*多表连接*/*/
 WHERE Student.Sno = SC.Sno AND SC.Cno = Course.Cno;
