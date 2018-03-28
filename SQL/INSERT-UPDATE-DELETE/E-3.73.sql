64719ef72be7d3aa52b3d3e59e330811775f0f35
David <8584751@qq.com>
2018-03-28 12:34:41 +0800

    Update E-3.73.sql

--- SQL/INSERT-UPDATE-DELETE/E-3.73.sql
+++ SQL/INSERT-UPDATE-DELETE/E-3.73.sql
@@ -1,1 +1,8 @@
-SELECT * FROM StudentUPDATE  StudentSET Sage=22WHERE  Sno='201215121';SELECT * FROM Student
+SELECT * FROM Student
+
+
+UPDATE  Student
+SET Sage=22
+WHERE  Sno='201215121';
+
+SELECT * FROM Student
