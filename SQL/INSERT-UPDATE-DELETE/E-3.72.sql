a7677276fc60005adcdf569f68464aaa35606444
David <8584751@qq.com>
2018-03-28 12:33:42 +0800

    Update E-3.72.sql

--- SQL/INSERT-UPDATE-DELETE/E-3.72.sql
+++ SQL/INSERT-UPDATE-DELETE/E-3.72.sql
@@ -1,1 +1,13 @@
---第一步：建表CREATE  TABLE  Dept_age( Sdept     CHAR(15),                     /*系名*/Avg_age SMALLINT);          	/*学生平均年龄*/--第二步：插入数据INSERTINTO  Dept_age(Sdept,Avg_age)SELECT  Sdept,AVG(Sage)FROM     StudentGROUP BY Sdept;SELECT * FROM Dept_age
+--绗竴姝ワ細寤鸿〃
+CREATE  TABLE  Dept_age
+( Sdept     CHAR(15),                     /*绯诲悕*/
+ Avg_age SMALLINT);          	/*瀛︾敓骞冲潎骞撮緞*/
+ 
+--绗簩姝ワ細鎻掑叆鏁版嵁
+INSERT
+INTO  Dept_age(Sdept,Avg_age)
+SELECT  Sdept,AVG(Sage)
+FROM     Student
+GROUP BY Sdept;
+
+SELECT * FROM Dept_age
