3ec015fa5139553bd54e4a418171740c2fe35dd3
David <8584751@qq.com>
2018-03-28 12:36:48 +0800

    Update E-3.48.sql

--- SQL/SELECT/E-3.48.sql
+++ SQL/SELECT/E-3.48.sql
@@ -1,1 +1,13 @@
-SELECT  Sno, AVG(Grade)FROM  SCGROUP BY SnoHAVING AVG(Grade)>=86;SELECT * FROM SC--错误的使用：--SELECT Sno, AVG(Grade)--FROM  SC--WHERE AVG(Grade)>=90--GROUP BY Sno;
+SELECT  Sno, AVG(Grade)
+FROM  SC
+GROUP BY Sno
+HAVING AVG(Grade)>=86;
+
+SELECT * FROM SC
+
+--错误的使用：
+
+--SELECT Sno, AVG(Grade)
+--FROM  SC
+--WHERE AVG(Grade)>=90
+--GROUP BY Sno;
