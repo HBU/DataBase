0e49f06ac63fbe2a629efddc50fe85fc06cc44c0
David <cmc_weiyg@163.com>
2019-03-13 11:37:12 +0800

    Update Create_Schema_Table.sql

--- SQL/CREATE-INSERT/Create_Schema_Table.sql
+++ SQL/CREATE-INSERT/Create_Schema_Table.sql
@@ -7,3 +7,11 @@ COL3 CHAR(20),
 COL4 NUMERIC(10,3),
 COL5 DECIMAL(5,2)
 );
+
+
+----------------------------
+删除模式，需要先删除该模式下的基本表，不能支持cascade
+例如：
+Drop table TEST.TAB1
+Drop SCHEMA TEST
+ref：https://zhidao.baidu.com/question/552968343.html
