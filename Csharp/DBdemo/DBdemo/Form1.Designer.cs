a81f2f7cc362724ebd5be7ac4600b42b44c5df10
David <8584751@qq.com>
2019-04-13 12:57:42 +0800

    add

---
+++ Csharp/DBdemo/DBdemo/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DBdemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.testDataSet = new DBdemo.TestDataSet();
+            this.sBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sTableAdapter = new DBdemo.TestDataSetTableAdapters.STableAdapter();
+            this.snoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.snameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ssexDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.snoDataGridViewTextBoxColumn,
+            this.snameDataGridViewTextBoxColumn,
+            this.sageDataGridViewTextBoxColumn,
+            this.ssexDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(79, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 177);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // testDataSet
+            // 
+            this.testDataSet.DataSetName = "TestDataSet";
+            this.testDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sBindingSource
+            // 
+            this.sBindingSource.DataMember = "S";
+            this.sBindingSource.DataSource = this.testDataSet;
+            // 
+            // sTableAdapter
+            // 
+            this.sTableAdapter.ClearBeforeFill = true;
+            // 
+            // snoDataGridViewTextBoxColumn
+            // 
+            this.snoDataGridViewTextBoxColumn.DataPropertyName = "Sno";
+            this.snoDataGridViewTextBoxColumn.HeaderText = "Sno";
+            this.snoDataGridViewTextBoxColumn.Name = "snoDataGridViewTextBoxColumn";
+            // 
+            // snameDataGridViewTextBoxColumn
+            // 
+            this.snameDataGridViewTextBoxColumn.DataPropertyName = "Sname";
+            this.snameDataGridViewTextBoxColumn.HeaderText = "Sname";
+            this.snameDataGridViewTextBoxColumn.Name = "snameDataGridViewTextBoxColumn";
+            // 
+            // sageDataGridViewTextBoxColumn
+            // 
+            this.sageDataGridViewTextBoxColumn.DataPropertyName = "Sage";
+            this.sageDataGridViewTextBoxColumn.HeaderText = "Sage";
+            this.sageDataGridViewTextBoxColumn.Name = "sageDataGridViewTextBoxColumn";
+            // 
+            // ssexDataGridViewTextBoxColumn
+            // 
+            this.ssexDataGridViewTextBoxColumn.DataPropertyName = "Ssex";
+            this.ssexDataGridViewTextBoxColumn.HeaderText = "Ssex";
+            this.ssexDataGridViewTextBoxColumn.Name = "ssexDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 314);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.testDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private TestDataSet testDataSet;
+        private System.Windows.Forms.BindingSource sBindingSource;
+        private TestDataSetTableAdapters.STableAdapter sTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn snameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ssexDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Csharp/DBdemo/DBdemo/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DBdemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 这行代码将数据加载到表“testDataSet.S”中。您可以根据需要移动或删除它。
+            this.sTableAdapter.Fill(this.testDataSet.S);
+
+        }
+    }
+}
