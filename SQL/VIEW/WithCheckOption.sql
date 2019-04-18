6c5334751c0a76112ee2b9a05e09f83b7aecf9a1
David <8584751@qq.com>
2019-04-18 17:44:24 +0800

    Create WithCheckOption.sql

---
+++ SQL/VIEW/WithCheckOption.sql
@@ -0,0 +1,26 @@
+create view View_Stu_1
+as
+select Sno,Sname,Ssex,Sage,Sdept
+from Student
+where Sdept='IS'
+
+go
+
+create view View_Stu_2
+as
+select Sno,Sname,Ssex,Sage,Sdept
+from Student
+where Sdept='IS'
+with check option;
+
+go
+
+insert into View_Stu_1(Sno,Sname,Ssex,Sage)--无With check option: Sdept会自动补充Null
+values ('95007111','张兰11','F','19')
+
+insert into View_Stu_2(Sno,Sname,Ssex,Sage,Sdept) --有With check option: 如果没有Sdept会报错，不能够为Sdept自动补充IS。
+values ('950072','李勇','F','19','IS')
+
+select * from Student
+select * from View_Stu_1
+select * from View_Stu_2
