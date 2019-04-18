2d883dfe689f06c590e49a020145f285551db8f6
David <8584751@qq.com>
2019-04-18 17:58:39 +0800

    Update WithCheckOption.sql

--- SQL/VIEW/WithCheckOption.sql
+++ SQL/VIEW/WithCheckOption.sql
@@ -1,4 +1,4 @@
-create view View_Stu_1
+﻿create view View_Stu_1
 as
 select Sno,Sname,Ssex,Sage,Sdept
 from Student
@@ -15,14 +15,17 @@ with check option;
 
 go
 
-insert into View_Stu_1(Sno,Sname,Ssex,Sage)--无With check option: Sdept会自动补充Null
+insert into View_Stu_1(Sno,Sname,Ssex,Sage)--无With check option: 执行正确。  Sdept会自动补充Null。
 values ('95007111','张兰11','F','19')
 
-insert into View_Stu_2(Sno,Sname,Ssex,Sage) --有With check option: 如果没有Sdept会报错，不能够为Sdept自动补充IS。
-values ('950072','李勇','F','19')
+insert into View_Stu_2(Sno,Sname,Ssex,Sage) --有With check option: 执行错误。  没有Sdept会报错，不能够为Sdept自动补充IS。
+values ('950072000','李勇00','F','19')
 
-insert into View_Stu_2(Sno,Sname,Ssex,Sage,Sdept) --有With check option: 有Sdept为IS，可以正常执行。
-values ('950072','李勇','F','19','IS')
+insert into View_Stu_2(Sno,Sname,Ssex,Sage,Sdept) --有With check option: 执行错误。有Sdept为CS，不正常执行。
+values ('951000071','王1五','F2','19','CS')
+
+insert into View_Stu_2(Sno,Sname,Ssex,Sage,Sdept) --有With check option: 执行正确。有Sdept为IS，可以正常执行。
+values ('95007211','李勇11','F','19','IS')
 
 select * from Student
 select * from View_Stu_1
