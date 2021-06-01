fed6b34555531e8e017ebefae5950eecbf29a7c2
David <cmc_weiyg@163.com>
2021-06-01 20:59:21 +0800

    1

---
+++ CsharpDB/ComboxTest/ComboxTest/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ComboxTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(168, 158);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(429, 158);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ CsharpDB/ComboxTest/ComboxTest/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+/*  程序功能：
+ *  加载时，从数据库读“省”的名字，写入combox1；combox1选项发生变化时，combox2填写内容
+ 
+ * 首先要在SQLserver中完成以下操作：
+ 
+1. 建立数据库 MrDavid
+2. 建立基本表 city
+    CREATE TABLE city(P_Name nchar(10) ,	C_Name nchar(10) ) 
+3.插入内容：
+    P_Name   C_Name
+    河北         保定        
+    河北        	石家庄       
+    河北        	唐山        
+    北京        	东城区       
+    北京        	西城区       
+    北京        	朝阳区       
+    天津        	河东区       
+    天津        	河西区       
+
+注意事项：数据库连接字符串要改成自己的相应参数
+*/
+
+
+
+namespace ComboxTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            load_Pname();//加载窗体时，combox1填写内容
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            load_Cname();//combox1选项发生变化时，combox2填写内容
+        }
+        
+        private void load_Pname()//从数据库读“省”的名字
+        {
+            ArrayList list = new ArrayList();
+            string conn = "Data Source=.;Initial Catalog=MrDavid;;User ID=sa;Password=sql"; //此处换位自己的数据库相关参数
+            string str = "select distinct P_Name from city ";
+            SqlDataAdapter da = new SqlDataAdapter(str, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            foreach (DataRow dr in dt.Rows)
+            {                
+                list.Add(dr[0].ToString().Trim());//dr[0]表示取结果的第一列，dr[1]就是第二列
+            }
+            comboBox1.DataSource = list;
+        }
+
+        private void load_Cname()//根据“省”的名字，从数据库读“市”的名字
+        {
+            String Pname = comboBox1.SelectedItem.ToString();
+            ArrayList list = new ArrayList();
+            string conn = "Data Source=.;Initial Catalog=MrDavid;;User ID=sa;Password=sql";//此处换位自己的数据库相关参数
+            string str = "select C_Name from city where P_Name = '" + Pname + "'";
+            SqlDataAdapter da = new SqlDataAdapter(str, conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            DataTable dt = ds.Tables[0];
+            foreach (DataRow dr in dt.Rows)
+            {
+                list.Add(dr[0].ToString().Trim());
+            }
+            comboBox2.DataSource = list;
+        }
+
+
+    }
+}
